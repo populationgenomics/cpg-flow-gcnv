3306693f0cb6a05d4f790056373cfe6660fc39bd
MattWellie <mattwellie@gmail.com>
2025-03-19 16:58:38 +1000

    stopping point, first stage done?

---
+++ src/cpg_gcnv/jobs/PrepareIntervals.py
@@ -0,0 +1,49 @@
+from typing import TYPE_CHECKING
+
+from cpg_utils.config import config_retrieve, image_path
+from cpg_utils.hail_batch import fasta_res_group, get_batch
+
+if TYPE_CHECKING:
+    from cpg_utils import Path
+    from hailtop.batch.job import BashJob, Job
+
+
+def prepare_intervals(job_attrs: dict[str, str], output_paths: dict[str, 'Path']) -> 'Job':
+    job = get_batch().new_job(
+        'Prepare intervals',
+        job_attrs | {'tool': 'gatk PreprocessIntervals/AnnotateIntervals'},
+    )
+    job.image(image_path('gatk_gcnv'))
+
+    reference = fasta_res_group(get_batch())
+
+    exclude_intervals_args = ' '.join([f'--exclude-intervals {i}' for i in config_retrieve(['workflow', 'exclude_intervals'], [])])
+
+    intervals = get_batch().read_input(config_retrieve(['workflow', 'intervals_path']))
+    job.command(f"""
+    gatk PreprocessIntervals \
+        --reference {reference.base} \
+        --intervals {intervals} \
+        {exclude_intervals_args} \
+        --padding 250 \
+        --bin-length 0 \
+        --interval-merging-rule OVERLAPPING_ONLY \
+        --output {job.preprocessed}
+    """)
+
+    job.command(f"""
+    gatk AnnotateIntervals \
+        --reference {reference.base} \
+        --intervals {job.preprocessed} \
+        --interval-merging-rule OVERLAPPING_ONLY \
+        --output {job.annotated}
+    """)
+
+    # Tell mypy the exact types of these Resources
+    job.preprocessed.add_extension('.interval_list')
+    job.annotated.add_extension('.tsv')
+
+    for key, path in output_paths.items():
+        get_batch().write_output(job[key], str(path))
+    return job
+
