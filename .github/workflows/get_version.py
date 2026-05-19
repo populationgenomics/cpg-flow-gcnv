e082b2e9822a2238f7f649f660f22d0b6aac3583
MattWellie <mattwellie@gmail.com>
2026-05-20 09:55:25 +1000

    pls

--- .github/workflows/get_version.py
+++ .github/workflows/get_version.py
@@ -26,44 +26,29 @@ def get_next_version_tag(folder: str, version: str) -> str:
         'GCP_BASE_IMAGE',
         'australia-southeast1-docker.pkg.dev/cpg-common/images',
     )
-    base_image_path_archive = os.environ.get(
-        'GCP_BASE_ARCHIVE_IMAGE',
-        'australia-southeast1-docker.pkg.dev/cpg-common/images-archive',
-    )
-    full_image_name_prod = f'{base_image_path_prod}/{folder}'
-    full_image_name_archive = f'{base_image_path_archive}/{folder}'
-
-    tags_list = []
-    for full_image_name in [full_image_name_prod, full_image_name_archive]:
-        cmd = [
-            'gcloud',
-            'container',
-            'images',
-            'list-tags',
-            full_image_name,
-            '--format=json',
-        ]
-        try:
-            result = subprocess.run(cmd, capture_output=True, text=True, check=True)  # noqa: S603
-        except subprocess.CalledProcessError:
-            continue
-
-        # If existing tags are found, proceed to determine the next version.
-        tags_list += json.loads(result.stdout)
+    cmd = [
+        'gcloud',
+        'container',
+        'images',
+        'list-tags',
+        f'{base_image_path_prod}/{folder}',
+        '--format=json',
+    ]
 
     max_suffix = 0
     pattern = re.compile(rf'^{re.escape(version)}-(\d+)$')
 
-    # If no tags are found, it returns the next version as -1.
-    for entry in tags_list:
-        tags = entry.get('tags', [])
-        for tag in tags:
+    try:
+        result = subprocess.run(cmd, capture_output=True, text=True, check=True)  # noqa: S603
+        for tag in json.loads(result.stdout)['tags']:
             match = pattern.match(tag)
             if match:
                 num = int(match.group(1))
                 max_suffix = max(max_suffix, num)
-    new_suffix = max_suffix + 1
-    return f'{version}-{new_suffix}'
+    except subprocess.CalledProcessError:
+        raise RuntimeError('Failed to list tags for the given image')
+
+    return f'{version}-{max_suffix + 1}'
 
 
 def main():
